71d1a3441149aef6712dbd18d0b05275161614cf
bashard001 <bdaseh@gmail.com>
2019-12-15 11:23:48 -0800

    started to create the classes i need for the app. created the main class of manager

---
+++ classes/manager.js
@@ -0,0 +1,26 @@
+class manager {
+    constructor (name, id, email, officeNumber){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.officeNumber = officeNumber;
+    }
+    getName(){
+        return this.name;
+    }
+    getId() {
+        return this.id;
+      }
+    
+      getEmail() {
+        return this.email;
+      }
+    
+      getRole() {
+        return "manager";
+      }
+    
+
+}
+
+module.exports = manager;
