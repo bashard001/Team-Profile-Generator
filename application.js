19cde3c262e505bd1f1bad7b0500d003bfdfdadf
bashard001 <bdaseh@gmail.com>
2019-12-15 11:45:30 -0800

    Created html templates that are needed to render the employee or team member information to and then to create the html page.

--- application.js
+++ application.js
@@ -1,10 +1,8 @@
 const fs = require("fs")
 const inquirer = require("inquirer")
+const path = require("path")
 let renderFile = require("./render")
-const generateManager = renderFile.createManager
-const generateEngineer = renderFile.createEngineer
-const generateIntern = renderFile.createIntern
-const renderHTML = renderFile.renderMain
+
 
 function askQuestions() {
 
